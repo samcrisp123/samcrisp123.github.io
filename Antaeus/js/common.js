237b5b8d0ab5ef8fade3b563c7feec77e4e7b94b
samcrisp123 <52202209+samcrisp123@users.noreply.github.com>
2019-07-02 12:49:36 +0100

    stc
    stc

---
+++ Antaeus/js/common.js
@@ -0,0 +1,9 @@
+class NavigationElement {
+
+  constructor(data) {
+    this.page = data.page;
+    this.title=data.title;
+    this.boxId=data.boxId;
+    this.secondaryBoxId=data.secondaryBoxId;
+  }
+}
